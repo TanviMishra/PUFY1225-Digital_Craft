977b363e04358378f298de9a683d9f99f991f362
TanviMishra <60146957+TanviMishra@users.noreply.github.com>
2020-03-21 22:07:09 +0530

    Update sketch.js

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,7 +1,7 @@
 let song;
 function preload(){
 //soundFormats('mp3', 'ogg');
- song = loadSound('/assets/Loyalty_Freak_Music_-_06_-_Take_A_Break');
+ song = loadSound('assets/Loyalty_Freak_Music_-_06_-_Take_A_Break');
 }
 
 function setup() {
