c2fed384b4e2a933e503d5503a153a3994478091
TanviMishra <60146957+TanviMishra@users.noreply.github.com>
2020-03-22 20:28:31 +0530

    Add files via upload

---
+++ wk1_sk2_2020_03_22_14_46_20/sketch.js
@@ -0,0 +1,24 @@
+let max_distance;
+
+function setup() {
+  createCanvas(710, 400);
+  noStroke();
+  max_distance = dist(0, 0, width, height);
+}
+
+function draw() {
+  //background(0);
+  //setting the dist between circles
+  for (let i = 0; i <= width; i += 20) {
+    for (let j = 0; j <= height; j += 20) {
+      //changing size as the mouse moves
+      let size = dist(mouseX, mouseY, i, j);
+      //resetting the size to a small integer
+      size = (size / max_distance) * 66;
+      //changes mouseX limits from 0-710 to 0-255
+      let c = map(mouseX, 0, width, 0, 255);
+      fill(255, c, 0)
+      rect(i, j, size, size);
+    }
+  }
+}
