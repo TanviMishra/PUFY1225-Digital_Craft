2b9e8a3418fed3ed38c85ace6260d9877aacbbae
TanviMishra <60146957+TanviMishra@users.noreply.github.com>
2020-03-22 21:30:21 +0530

    Add files via upload

---
+++ music1/sketch.js
@@ -0,0 +1,15 @@
+let song;
+function preload(){
+soundFormats('mp3');
+song = loadSound('Loyalty_Freak_Music_-_06_-_Take_A_Break.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);  
+  song.play();
+}
+
+function draw() {
+background("GREEN");
+
+  }
