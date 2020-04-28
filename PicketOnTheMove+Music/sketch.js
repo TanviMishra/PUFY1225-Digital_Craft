e5d78ce35217e75527e11fefea24124144b6c6ce
TanviMishra <60146957+TanviMishra@users.noreply.github.com>
2020-04-28 20:26:17 +0530

    Update sketch.js

--- PicketOnTheMove+Music/sketch.js
+++ PicketOnTheMove+Music/sketch.js
@@ -18,7 +18,7 @@ function setup() {
   fr = ((bpm.get(15,2))/60);
   frameRate(fr);
   cam = createCamera();
-  song.play();
+  
   
   for(i=0;i<1;i++){
   picket[i] = new Picket(0,val1[count],0);  
@@ -35,7 +35,7 @@ function setup() {
 }
 
 function draw() {
-  
+  song.play();
   background(255,0,random(100, 200));
   
    cam.move(delta, 0, 0);
